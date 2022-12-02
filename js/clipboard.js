ceca53bb64878428ab6f00f8deb5b24bafa42e0b
BaiFeather白羽 <60559142+BiliZheXue@users.noreply.github.com>
2022-12-02 11:33:54 +0800

    Add files via upload

---
+++ js/clipboard.js
@@ -0,0 +1,16 @@
+<script> 
+        function copyToClipboard(s) {
+            if (window.clipboardData) {
+                window.clipboardData.setData(\'text\', s);
+            } else {
+                (function(s) {
+                    document.oncopy = function(e) {
+                        e.clipboardData.setData(\'text\', s);
+                        e.preventDefault();
+                        document.oncopy = null;
+                    }
+                })(s);
+                document.execCommand(\'Copy\');
+            }
+        }
+    </script>
